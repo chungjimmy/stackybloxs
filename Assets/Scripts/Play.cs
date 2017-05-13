4255850a13e1d223f8a07d7fc7b904c1b7e1aec3
yuninghong <kitmenghong@gmail.com>
2017-05-13 00:27:49 -0700

    homescreen

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+	public GameObject Home;
+
+	public GameObject transition;
+
+	public void StartGame(){
+		transition.SetActive(true);
+		Home.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+	public GameObject player;
+
+	public GameObject platform;
+
+	public GameObject loseLine;
+
+	public GameObject background1;
+
+	public GameObject itemBox;
+
+	public GameObject score;
+
+	public GameObject logo;
+
+	public GameObject manager;
+
+	public float delay;
+
+	// Use this for initialization
+	void Start () {
+		delay = 0f;
+		Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		delay += Time.deltaTime;
+		if(delay > 1f){
+			logo.SetActive(false);
+			manager.gameObject.GetComponent<Tutorial>().enabled = true;
+			player.SetActive(true);
+			platform.SetActive(true);
+			loseLine.SetActive(true);
+			background1.SetActive(true);
+			itemBox.SetActive(true);
+			score.SetActive(true);
+		}
+	}
+}
